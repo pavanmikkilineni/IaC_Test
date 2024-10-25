deac62fa83501f518578f61e1332cf3ee8b06021
MIKKILINENI PAVAN KUMAR <74001609+pavanmikkilineni@users.noreply.github.com>
2024-10-25 14:18:31 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -93,4 +93,15 @@ output "public_ip" {
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-public-bucket"
   acl    = "public-read"
+
+   logging {
+    target_bucket = "my-unique-log-bucket" # Change to your unique log bucket name
+    target_prefix = "log/"
+  }
+
+}
+
+resource "aws_s3_bucket" "my_log_bucket" {
+  bucket = "my-unique-log-bucket" # Change to your unique log bucket name
+  acl    = "private"
 }
